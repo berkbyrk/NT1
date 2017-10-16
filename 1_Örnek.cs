f140d9fea397b89156f4c36d3712785ef72f624d
berkbyrk <32482029+berkbyrk@users.noreply.github.com>
2017-10-16 19:48:06 +0300

    1_Örnek

---
+++ 1_Örnek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+kullanarak System;
+
+ad alanı  omer {
+
+   sınıf BirinciOrnek
+{
+
+
+           static void Ana(String[] args)
+{
+    string reklam, soyad;
+    ad = " Berk ";
+    soyad = " Bayrak ";
+
+    Console.WriteLine(ad + "  " + soyad);
+
+    Console.ReadKey();
+
+}
+
+
+   }
+
+}
